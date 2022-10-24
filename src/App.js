c983a109408cd5d90da88ec21defa0cc51dd05e0
Laura Clobochar Pereira <clo.bochar_@hotmail.com>
2022-10-24 18:06:55 +0100

    New code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { todos as data } from "./api/data";
 
 function App() {
+  const [todos, setTodos] = useState(data);
+  const [input, setInput] = useState("");
+
+  const handleClick = () => {
+    setTodos((prev) => {
+      const count = prev.length + 1;
+      return [...prev, { title: input, id: count }];
+    });
+    setInput("");
+  };
+
+  const handleDelete = (id) => {
+    setTodos((prev) => {
+      return prev.filter((item) => item.id !== id);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          placeholder="enter your task.."
+        />
+        <button onClick={handleClick}>Save</button>
+        <h2>{input}</h2>
+      </div>
+      <div className="list-container">
+        <Button name="test" style={{ color: "purple" }} />
+        <ul className="list">
+          {todos.map((todo) => {
+            return (
+              <li key={`todo-${todo.id}`}>
+                {todo.id}-{todo.title}
+                <button onClick={() => handleDelete(todo.id)}>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
+}
+
+function Button({ name, style }) {
+  return <button style={Object.assign({}, style)}>{name}</button>;
 }
 
 export default App;
---
+++ src/api/data.js
@@ -0,0 +1,14 @@
+export let todos = [
+  {
+    id: 1,
+    title: "buy milk",
+  },
+  {
+    id: 2,
+    title: "pay bill",
+  },
+  {
+    id: 3,
+    title: "send flowers",
+  },
+];
